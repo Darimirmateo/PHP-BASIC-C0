e89c897ac67329419dde28109e69e4338a78878c
Darimir Mateo <darimir.mateo@student.rocva.nl>
2020-12-03 15:02:32 +0100

    almost done

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -1,4 +1,37 @@
 <?php
 
+//eerste opdracht
+//echo "Hallo en Welkom!";
+//echo '<h1>Darimir</h1>';
 
-echo "Hallo en Welkom!";
+
+//tweedeopdracht
+//$score = 0;
+
+
+
+
+//3deopdracht
+
+///if($naam = " darimirmateo")
+//{
+//    echo "hey topper";
+//}
+
+
+//4deopdracht
+if($leeftijd = "27")
+{
+    echo "27";
+}
+
+
+$Pakjemelk = 10.365;
+var_dump($Pakjemelk);
+
+
+
+?>
+
+ 
+
