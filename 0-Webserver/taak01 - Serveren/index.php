957be9f0a8e3b7001aa8ecfb644dffceac8fc8e9
Darimir Mateo <darimir.mateo@student.rocva.nl>
2020-12-03 15:42:53 +0100

    taak08 done!!! lets goo

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -19,14 +19,52 @@
 
 
 //taak3
-if($leeftijd = "27")
-{
-    echo "leettijd is 27" ;
-}
+//if($leeftijd = "27")
+//{
+  //  echo "leettijd is 27" ;
+//}
 
 //tak03
-$Pakjemelk = 10.365;
-var_dump($Pakjemelk);
+//$Pakjemelk = 10.365;
+//var_dump($Pakjemelk);
+
+
+//taak04
+//$x = "0";
+//$x = 1;
+//var_dump($x);
+
+
+//taak05
+//$voornaam = "darimir";
+//echo gettype($voornaam);
+
+
+//taak06
+//$voornaam = "darimir";
+//echo gettype($voornaam);
+
+//$x = "0";
+//$x = 1;
+//var_dump ($x);
+
+//$Pakjemelk = 10.365;
+//var_dump ($Pakjemelk); 
+
+//taak07
+//$voornaam = "Darimir";
+//$leeftijd = 26;
+//echo "Jouw naam is $voornaam en je bent $leeftijd jaar oud.";
+
+
+
+//taak08
+echo "<h2>VOORNAMEN ?</h2>";
+echo "voornaam 1<br>";
+echo "voornaam 2<br>";
+echo "voornaam 3<br>";
+
+
 
 
 
