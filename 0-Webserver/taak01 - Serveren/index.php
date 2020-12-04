5a9b6b74e0c0127403a1b16258224d4d427eca54
Darimir Mateo <darimir.mateo@student.rocva.nl>
2020-12-04 12:25:01 +0100

    php basic bijna klaar alles aantowrden hierdaarna uitwerken!

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -86,13 +86,106 @@
  // $myMovies = ["Spiderman", "Batman", "The Joker"];
  // echo'. Spiderman <br>. Batman<br>. The Joker <br>';
 
-  
 
+ //PHP-BASIC-C0 | taak: taak02 - Array waarde wijzigen
 
-
-
+ //$mydognames =  ['pedro', 'jose', 'juan'];
+  //echo 'my dog names <br>'. $mydognames [2];
 
     
+ // taak: taak03 - Array waarde toevoegen
+// $myheroes =  ['spiderman', 'Ironman', 'deadpool'];
+//   array_push($myheroes, 'wonder woman');
+  //  print_r ($myheroes);
+   
+
+// taak: taak04 - Multidimensionaal
+
+//$abcgetallen = [ [1,2,3,4], ["a", "b", "c", "d"], true, false  ];
+//array_push($abcgetallen, '24,56,78,90');
+
+//print_r ($abcgetallen);
+// echo $abcgetallen = "a46 <br>","c90 <br>";
+
+//taak: taak05 - Array sorteren
+//$laaghoog =  ["676","5345","78657","3","46","21","47",];
+ // asort ($laaghoog);
+ // print_r ($laaghoog);
+
+ //$hooglaag =  ["676","5345","78657","3","46","21","47",];
+ // arsort ($hooglaag);
+ // print_r ($hooglaag);
+
+
+ //taak01 - Arithmetic Operators
+
+   //$getal1 = 10;
+   //$getal2 = 10;
+   // echo $getal1 + $getal2; 
+
+    //$getal1 = 5;
+    //$getal2 = 10;
+    //echo $getal1 - $getal2; 
+
+    //$getal1 = 15;
+    //$getal2 = 10;
+    //echo $getal1 * $getal2;
+
+    //$getal1 = 5;
+    //$getal2 = 10;
+    //echo $getal1 / $getal2;
+
+
+   // taak02 -Assignment Operators
+
+
+ //    $getal1 += 30;
+ //    $getal1 -= 50;
+//     $getal1 *= 50;
+ //    $getal1 /= 2;
+
+ //    echo $getal1;
+
+
+ 
+ //taak: taak03 - Increment-operators
+
+ //$getal1 = 10;
+ //echo $getal1++;
+
+ //$getal1 = 15;
+ //echo ++$getal1;
+ 
+ 
+
+
+ 
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
