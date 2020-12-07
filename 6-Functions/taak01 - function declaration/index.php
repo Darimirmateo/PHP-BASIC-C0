270b964150766cf586757c4cc5d99a7e31c8ddf1
Darimir Mateo <darimir.mateo@student.rocva.nl>
2020-12-07 12:07:21 +0100

    zoekletter

---
+++ 6-Functions/taak01 - function declaration/index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+function zoekLetterInAlfabet (){
+    $woordje  = "Hallo";
+    $zinnetje =  "mooie blauwe planeet!";
+    $helezin = "Hallo mooie blauwe planeet";
+   
+   
+
+
+
+
+}
+
+zoekLetterInAlfabet();
+
+
+
+
+
+
+?>
