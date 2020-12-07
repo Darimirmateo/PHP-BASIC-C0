1c64ddbbb5518c45f450970699595569075c3207
Darimir Mateo <darimir.mateo@student.rocva.nl>
2020-12-07 10:33:49 +0100

    done identiek als

--- 5-Conditional_Statements/taak02 - if conditional/index.php
+++ 5-Conditional_Statements/taak02 - if conditional/index.php
@@ -3,9 +3,36 @@
 $test1 = 16;
 $test2 = 6;
 
-if($variabele1 == $variabele2) {
-    echo "gelijk!";
-}
+if($test1== $test2);
+    echo  "<p>gelijk!</P>";
+
+
+if($test1 != $test2);
+    echo  "<p>ongelijk!</p>";   
+
+
+if($test1=== $test2);
+    echo  "<P> identiek! </p>";
+
+
+if($test1 > $test2);
+    echo "<p>test1 is groter dan test2</p>";  
+
+
+if($test1 < $test2);
+    echo "<p>test1 is kleiner dan test2</p>";   
+    
+if($test1 >= $test2);
+    echo "<p>test1 is groter of gelijk aan test2</p>";      
+
+
+if($test1 <= $test2);
+    echo "<p>test1 is kleiner of gelijk aan test2</p>";     
+
+
+
+    
+
 
 
 
---
+++ 5-Conditional_Statements/taak03 - if, else conditional/index.php
@@ -0,0 +1,49 @@
+<?php
+
+
+$test1 = 3;
+$test2 = 6;
+
+if($test1 == $test2) {
+    echo  "gelijk!";
+}
+
+else {
+    echo "niet gelijk!";
+}
+
+
+if($test1 != $test2) {
+    echo  "ongelijk!";
+}
+
+else {
+    echo  "ongelijk aan!";
+}
+
+
+
+
+if($test1 === $test2) {
+    echo "iedentiek aan";
+}
+
+else {
+echo "niet iedentiek";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
