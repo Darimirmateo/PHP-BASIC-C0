e4bfc94289fe13fd65995b1e9c75218fabeb5339
Darimir Mateo <darimir.mateo@student.rocva.nl>
2020-12-08 11:14:40 +0100

    working

---
+++ 7-Working with files/index.php
@@ -0,0 +1,10 @@
+<html>
+
+
+
+    <div class="menu">
+        <?php include 'C:\xampp\htdocs\php-basic-c0\7-Working with files\taak-01 - include\menu.php';?>
+    </div>
+
+
+</html>
---
+++ 7-Working with files/taak01 - include/menu.php
@@ -0,0 +1,9 @@
+<?php
+
+<html>
+<head>
+     <title></title>
+     <?php include '\taak01 - include\index.php';?>
+</head>
+
+?>
